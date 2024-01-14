19290baf3660fcb226eb53556686fe07108738d1
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-01-13 23:47:54 -0600

    Update specifications section in styles and creation of the same

--- src/js/main.js
+++ src/js/main.js
@@ -47,68 +47,95 @@ fetch("data.json")
       updateDOM();
     });
 
-    const specificationSection = document.createElement("section");
-    specificationSection.classList.add("specification-section");
+    const createSpecificationSection = (element) => {
+      searchContainer.classList.add("hidden");
+      filterContainer.classList.add("hidden");
+      containerCards.classList.add("hidden");
 
-    const containerBack = document.createElement("div");
-    containerBack.classList.add("specification-section__back");
+      const specificationSection = document.createElement("section");
+      specificationSection.classList.add("specification-section");
 
-    const backIcon = document.createElement("i");
-    backIcon.classList.add("fa-solid", "fa-arrow-left");
+      specificationSection.style.display = "flex";
 
-    const paragraphBack = document.createElement("p");
-    paragraphBack.textContent = "Back";
+      const containerBack = document.createElement("div");
+      containerBack.classList.add("specification-section__back");
 
-    containerBack.addEventListener("click", () => {
-      containerCards.classList.remove("hidden");
-      specificationSection.remove();
-    })
+      const backIcon = document.createElement("i");
+      backIcon.classList.add("fa-solid", "fa-arrow-left");
 
-    const imageSpecification = document.createElement("img");
-    imageSpecification.classList.add("specification-section__img");
+      const paragraphBack = document.createElement("p");
+      paragraphBack.textContent = "Back";
 
-    const titleSpecification = document.createElement("h2");
-    titleSpecification.classList.add("specification-section__title");
+      containerBack.addEventListener("click", () => {
+        containerCards.classList.remove("hidden");
+        searchContainer.classList.remove("hidden");
+        filterContainer.classList.remove("hidden");
+        specificationSection.remove();
+      })
 
-    const textSpecification = document.createElement("div");
-    textSpecification.classList.add("specification-section__text");
+      const imageSpecification = document.createElement("img");
+      imageSpecification.classList.add("specification-section__img");
 
-    const textSpecificationDate1 = document.createElement("div");
+      const titleSpecification = document.createElement("h2");
+      titleSpecification.classList.add("specification-section__title");
 
-    const NativeName = document.createElement("p");
-    const Population = document.createElement("p");
-    const Region = document.createElement("p");
-    const Subregion = document.createElement("p");
-    const Capital = document.createElement("p");
+      const textSpecification = document.createElement("div");
+      textSpecification.classList.add("specification-section__text");
 
-    const textSpecificationDate2 = document.createElement("div");
+      const textSpecificationDate1 = document.createElement("div");
 
-    const TopLevelDomain = document.createElement("p");
-    const Currencies = document.createElement("p");
-    const Languages = document.createElement("p");
+      const NativeName = document.createElement("p");
+      const Population = document.createElement("p");
+      const Region = document.createElement("p");
+      const Subregion = document.createElement("p");
+      const Capital = document.createElement("p");
 
-    const borderCountries = document.createElement("div");
-    borderCountries.classList.add("specification-section__border-countries");
+      const textSpecificationDate2 = document.createElement("div");
 
-    const borderCountriesItem1 = document.createElement("button");
-    borderCountriesItem1.classList.add("specification-section__border-countries__item");
-    const borderCountriesItem2 = document.createElement("button");
-    borderCountriesItem2.classList.add("specification-section__border-countries__item");
-    const borderCountriesItem3 = document.createElement("button");
-    borderCountriesItem3.classList.add("specification-section__border-countries__item");
+      const TopLevelDomain = document.createElement("p");
+      const Currencies = document.createElement("p");
+      const Languages = document.createElement("p");
 
-    mainContainer.appendChild(specificationSection);
-    specificationSection.append(containerBack, imageSpecification, titleSpecification, textSpecification, borderCountries);
+      const borderCountries = document.createElement("div");
+      borderCountries.classList.add("specification-section__border-countries");
 
-    containerBack.append(backIcon, paragraphBack);
+      const borderCountriesItem1 = document.createElement("button");
+      borderCountriesItem1.classList.add("specification-section__border-countries__item");
+      const borderCountriesItem2 = document.createElement("button");
+      borderCountriesItem2.classList.add("specification-section__border-countries__item");
+      const borderCountriesItem3 = document.createElement("button");
+      borderCountriesItem3.classList.add("specification-section__border-countries__item");
 
-    textSpecification.append(textSpecificationDate1, textSpecificationDate2);
+      imageSpecification.src = element.flags.svg;
+      titleSpecification.textContent = element.name;
 
-    textSpecificationDate1.append(NativeName, Population, Region, Subregion, Capital);
+      NativeName.innerHTML = `<strong>Native Name:</strong> ${element.nativeName}`;
+      Population.innerHTML = `<strong>Population:</strong> ${element.population}`;
+      Region.innerHTML = `<strong>Region:</strong> ${element.region}`;
+      Subregion.innerHTML = `<strong>Sub Region:</strong> ${element.subregion}`;
+      Capital.innerHTML = `<strong>Capital:</strong> ${element.capital}`;
 
-    textSpecificationDate2.append(TopLevelDomain, Currencies, Languages);
+      TopLevelDomain.innerHTML = `<strong>Top Level Domain:</strong> ${element.topLevelDomain}`;
+      Currencies.innerHTML = `<strong>Currencies:</strong> ${element.currencies[0].name}`;
+      Languages.innerHTML = `<strong>Languages:</strong> ${element.languages.map(lang => lang.name).join(", ")}`;
 
-    borderCountries.append(borderCountriesItem1, borderCountriesItem2, borderCountriesItem3);
+      borderCountriesItem1.textContent = "France";
+      borderCountriesItem2.textContent = "Germany";
+      borderCountriesItem3.textContent = "Netherlands";
+
+      mainContainer.appendChild(specificationSection);
+      specificationSection.append(containerBack, imageSpecification, textSpecification, borderCountries);
+
+      containerBack.append(backIcon, paragraphBack);
+
+      textSpecification.append(titleSpecification, textSpecificationDate1, textSpecificationDate2);
+
+      textSpecificationDate1.append(NativeName, Population, Region, Subregion, Capital);
+
+      textSpecificationDate2.append(TopLevelDomain, Currencies, Languages);
+
+      borderCountries.append(borderCountriesItem1, borderCountriesItem2, borderCountriesItem3);
+    }
 
     const updateDOM = () => {
       const cardFragment = document.createDocumentFragment();
@@ -117,25 +144,7 @@ fetch("data.json")
         const card = document.createElement("div");
         card.classList.add("country-cards__card");
 
-        card.addEventListener("click", () => {
-          searchContainer.classList.add("hidden");
-          filterContainer.classList.add("hidden");
-          containerCards.classList.add("hidden");
-          specificationSection.style.display = "flex";
-
-          imageSpecification.src = element.flags.svg;
-          titleSpecification.textContent = element.name;
-
-          NativeName.innerHTML = `<strong>Native Name:</strong> ${element.nativeName}`;
-          Population.innerHTML = `<strong>Population:</strong> ${element.population}`;
-          Region.innerHTML = `<strong>Region:</strong> ${element.region}`;
-          Subregion.innerHTML = `<strong>Sub Region:</strong> ${element.subregion}`;
-          Capital.innerHTML = `<strong>Capital:</strong> ${element.capital}`;
-
-          TopLevelDomain.innerHTML = `<strong>Top Level Domain:</strong> ${element.topLevelDomain}`;
-          Currencies.innerHTML = `<strong>Currencies:</strong> ${element.currencies[0].name}`;
-          Languages.innerHTML = `<strong>Languages:</strong> ${element.languages[0].name}`;
-        });
+        card.addEventListener("click", () => createSpecificationSection(element));
 
         const img = document.createElement("img");
         img.classList.add("country-cards__card__img");
